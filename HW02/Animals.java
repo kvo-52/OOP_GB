e2c65a800a5ff1f667b2046691dd4a271a74ef36
kvo.52@mail.ru <kvo.52@mail.ru>
2023-03-13 20:54:49 +0300

    add interface Animals

---
+++ HW02/Animals.java
@@ -0,0 +1,11 @@
+public interface Animal {
+    double getHeight(); // Рост животного
+
+    double getWeight(); // Вес животного
+
+    String getEyeColor(); // Цвет глаз животного
+
+    void makeSound(); // Издать звук
+
+    void printInfo(); // Напечатать информацию о животном
+}
